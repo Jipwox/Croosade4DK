bb4272298de6b3f1b7bdb25c6eadcf9b875ff770
Jipwox <johnphilip.cardi@gmail.com>
2020-08-26 11:36:18 -0700

    added working checkboxes

--- croosade_4dk/lib/Pages/AddCrusadeBattlePage.dart
+++ croosade_4dk/lib/Pages/AddCrusadeBattlePage.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import '../Models/CrusadeBattleModel.dart';
 import '../Models/CrusadeCardModel.dart';
@@ -19,6 +20,20 @@ class _AddCrusadeBattleState extends State<AddCrusadeBattlePage>{
   TextEditingController infoController = new TextEditingController();
   TextEditingController date = new TextEditingController();
   List<CrusadeCardModel> cardModels = new List<CrusadeCardModel>();
+  DateTime selectedDate = DateTime.now();
+  Set<int> checkedIds = new Set<int>();
+  Map<int,bool> checkedValues = new Map<int,bool>();
+
+  Future<List<CrusadeCardModel>> initRetrieveCardModels() async {
+    cardModels = await DatabaseProvider.db.getCrusadeCards(widget.crusadeForceId);
+
+    cardModels.forEach((element) {
+      checkedIds.add(element.id);
+      checkedValues[element.id] = true;
+    });
+
+    return cardModels;
+  }
 
   Future<List<CrusadeCardModel>> retrieveCardModels() async {
     cardModels = await DatabaseProvider.db.getCrusadeCards(widget.crusadeForceId);
@@ -26,10 +41,23 @@ class _AddCrusadeBattleState extends State<AddCrusadeBattlePage>{
     return cardModels;
   }
 
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2015, 8),
+        lastDate: DateTime(2101));
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
+  }
+
+
   @override
   void initState(){
     super.initState();
-    retrieveCardModels();
+    initRetrieveCardModels();
   }
 
   void refreshPage(){
@@ -55,7 +83,7 @@ class _AddCrusadeBattleState extends State<AddCrusadeBattlePage>{
                           children: [
                             Padding(padding: EdgeInsets.only(top: 10.0, left: 20.0),),
                             Text("Battle of "),
-                            SizedBox(width: 40,),
+                            SizedBox(width: 80,),
                             Container(
                               width: 250.0,
                               child: TextField(
@@ -66,9 +94,37 @@ class _AddCrusadeBattleState extends State<AddCrusadeBattlePage>{
                             ),
                           ]
                       ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(padding: EdgeInsets.only(top: 10.0, left: 20.0),),
+                            Text("Opposing Force "),
+                            SizedBox(width: 35,),
+                            Container(
+                              width: 250.0,
+                              child: TextField(
+                                decoration: InputDecoration(
+                                ),
+                                controller: opposingForceNameController,
+                              ),
+                            ),
+                          ]
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(padding: EdgeInsets.only(top: 10.0, left: 20.0),),
+                            Text("Date "),
+                            SizedBox(width: 105,),
+                            Text("${selectedDate.month}/${selectedDate.day}/${selectedDate.year}"),
+                            IconButton(
+                              icon: Icon(Icons.date_range),
+                              onPressed: () => _selectDate(context),
+                            ),
+                          ]
+                      ),
                       ListView.separated(
                         shrinkWrap: true,
-                        physics: new NeverScrollableScrollPhysics(),
                         padding: EdgeInsets.all(16.0),
                         itemCount: cardModels.length,
                         itemBuilder: /*1*/ (context, i) {
@@ -76,7 +132,30 @@ class _AddCrusadeBattleState extends State<AddCrusadeBattlePage>{
                         },
                         separatorBuilder: (context, index) {
                           return Divider();
-                        },)
+                        },),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Padding(padding: EdgeInsets.only(top: 10.0, left: 20.0),),
+                            Container(
+                              width: 300,
+                              child: TextField(
+                                maxLines: 5,
+                                decoration: InputDecoration(
+                                  hintText: "Battle details / notable events...",
+                                  border: OutlineInputBorder(),
+                                ),
+                                controller: infoController,
+                              ),
+                            ),
+                          ]
+                      ),
+                      RaisedButton(
+                        child: Text("Save"),
+                        onPressed: () => {
+
+                        },
+                      ),
                     ],
                   )
               );
@@ -88,44 +167,18 @@ class _AddCrusadeBattleState extends State<AddCrusadeBattlePage>{
 
   Widget _buildRow(CrusadeCardModel cardModel) {
     String title = "${cardModel.name} / PR: ${cardModel.powerRating} / ${cardModel.rank}";
-    return ListTile(
+    return CheckboxListTile(
       title: Text(title),
-      trailing: IconButton(
-        icon: Icon(
-          Icons.highlight_off,
-        ),
-        onPressed: (){
-          showDialog(
-              barrierDismissible: true,
-              context: context,
-              builder: (BuildContext context) {
-                return AlertDialog (
-                  title: Text("Are you sure you want to delete $title ?"),
-                  actions: [
-                    FlatButton(
-                      child: Text("No"),
-                      onPressed: () {
-                        Navigator.of(context).pop(true); //supposedly the "true" param will refresh the UI on pop
-                      },
-                    ),
-                    FlatButton(
-                      child: Text("Yes"),
-                      onPressed: () {
-                        DatabaseProvider.db.deleteCrusadeCardModel(cardModel.id, cardModel.crusadeForceId);
-                        Navigator.of(context).pop(true); //supposedly the "true" param will refresh the UI on pop
-                      },
-                    ),
-                  ],
-                );
-              }
-          ).then((value) => {
-            setState(() {
-            })
-          });
-        },
-      ),
-      onTap: () {
-        //_navigateToViewCrusadeCardPage(context, cardModel.id);
+      controlAffinity: ListTileControlAffinity.leading,
+      value: checkedValues[cardModel.id],
+      onChanged: (bool value){
+        print(value);
+        setState(() {
+          checkedValues[cardModel.id] = value;
+          if(value) checkedIds.add(cardModel.id);
+          if(!value) checkedIds.remove(cardModel.id);
+          print(checkedIds);
+        });
       },
     );
   }
